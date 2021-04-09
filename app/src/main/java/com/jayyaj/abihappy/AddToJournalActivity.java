a2e9f832310da740ed835f64a798a27340150ac6
1jayyaj1 <jayabisaad@gmail.com>
2021-04-09 13:42:22 -0400

    Create account implemented but not working

---
+++ app/src/main/java/com/jayyaj/abihappy/AddToJournalActivity.java
@@ -0,0 +1,31 @@
+package com.jayyaj.abihappy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+public class AddToJournalActivity extends AppCompatActivity {
+    private TextView test;
+    private TextView test2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_journal);
+
+//        test = findViewById(R.id.test);
+//        test2 = findViewById(R.id.test2);
+//
+//        Intent intent = getIntent();
+//        String username = intent.getStringExtra("username");
+//        String userId = intent.getStringExtra("userId");
+//
+//        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(userId)) {
+//            test.setText(username);
+//            test2.setText(userId);
+//        }
+    }
+}
--- app/src/main/java/com/jayyaj/abihappy/LoginActivity.java
+++ app/src/main/java/com/jayyaj/abihappy/LoginActivity.java
@@ -2,13 +2,29 @@ package com.jayyaj.abihappy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signup = findViewById(R.id.createAccountSignInButton);
+
+        signup.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+        });
     }
 }
