4164f03443ac74dcd5411d17b48b89f0c298db15
1jayyaj1 <jayabisaad@gmail.com>
2021-04-11 12:07:36 -0400

    Added menu items

--- app/src/main/java/com/jayyaj/abihappy/JournalTimelineActivity.java
+++ app/src/main/java/com/jayyaj/abihappy/JournalTimelineActivity.java
@@ -1,8 +1,11 @@
 package com.jayyaj.abihappy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class JournalTimelineActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class JournalTimelineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_timeline);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.actionAdd:
+                break;
+            case R.id.actionSignOut:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jayyaj/abihappy/LoginActivity.java
+++ app/src/main/java/com/jayyaj/abihappy/LoginActivity.java
@@ -1,20 +1,33 @@
 package com.jayyaj.abihappy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.jayyaj.abihappy.util.JournalApi;
 
 public class LoginActivity extends AppCompatActivity {
     private Button signup;
@@ -53,5 +66,31 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void signinEmailPasswordUser(String email, String password) {
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                        assert user != null;
+                        String currentUserId = user.getUid();
+                        collectionReference.whereEqualTo("userId", currentUserId)
+                                .addSnapshotListener((queryDocumentSnapshots, error) -> {
+                            if (error != null) {}
+                            assert queryDocumentSnapshots != null;
+                            if (!queryDocumentSnapshots.isEmpty()) {
+                                for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                    JournalApi journalApi = JournalApi.getInstance();
+                                    journalApi.setUsername(snapshot.getString("username"));
+                                    journalApi.setUserId(currentUserId);
+
+                                    startActivity(new Intent(LoginActivity.this, AddToJournalActivity.class));
+                                }
+                            }
+                        });
+            }).addOnFailureListener(e -> {
+
+            });
+        } else {
+            Toast.makeText(LoginActivity.this, "Please enter an email and a password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
